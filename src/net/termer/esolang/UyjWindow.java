3cbd10b2c870c010eeb769716af0bf6c8ab630bf
Termer <termer.net@gmail.com>
2018-02-22 23:28:18 +0000

    IDE finished

---
+++ src/net/termer/esolang/UyjWindow.java
@@ -0,0 +1,136 @@
+package net.termer.esolang;
+
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.FlowLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JProgressBar;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+import net.termer.esolang.exception.CommandParseException;
+import net.termer.esolang.exception.DataTypeMismatchException;
+import net.termer.esolang.exception.GotoException;
+import net.termer.esolang.exception.VariableException;
+
+public class UyjWindow extends JFrame {
+	public static UyjWindow w = null;
+	
+	private JTextArea top = null;
+	public JTextArea bottom = null;
+	private JProgressBar secondsBar = null;
+	private JButton interpret = null;
+	private int codeTimer = 0;
+	
+	private UyjWindow() {
+		//Initialize and setup some stuff
+		super("jUyjhmn n");
+		setSize(640,500);
+		getContentPane().setBackground(Color.RED);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		getContentPane().setLayout(new FlowLayout());
+		setResizable(false);
+		
+		//Add components
+		top = new JTextArea(18,55);
+		top.setEditable(false);
+		top.setBackground(Color.GREEN);
+		top.setSelectedTextColor(Color.ORANGE);
+		top.setSelectionColor(Color.BLUE);
+		
+		bottom = new JTextArea(10,26);
+		bottom.setBackground(Color.WHITE);
+		bottom.setForeground(Color.MAGENTA);
+		bottom.setSelectedTextColor(Color.CYAN);
+		bottom.setSelectionColor(Color.PINK);
+		bottom.setLineWrap(false);
+		bottom.setCursor(new Cursor(Cursor.CROSSHAIR_CURSOR));
+		
+		secondsBar = new JProgressBar();
+		secondsBar.setMinimum(0);
+		secondsBar.setMaximum(100);
+		secondsBar.setValue(0);
+		secondsBar.setStringPainted(true);
+		new CodeTimer().start();
+		
+		interpret = new JButton("<html><b><s>JUST INTERPRET MY CODE ALREADY!!!</s></b></html>");
+		interpret.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				try {
+					if(OutputWindow.initialized()) {
+						OutputWindow.clear();
+					}
+					new Interpreter(bottom.getText()).interpret(Uyjhmnn.ignoreErrors);
+				} catch (DataTypeMismatchException | VariableException
+						| CommandParseException | GotoException e) {
+					Uyjhmnn.output("\nScript error!!1!");
+					Uyjhmnn.output("\n"+e.getClass().getName()+": "+e.getMessage());
+					if(codeTimer*2<100) {
+						codeTimer=codeTimer*2;
+					} else {
+						codeTimer=99;
+					}
+				}
+			}
+		});
+		
+		getContentPane().add(new JScrollPane(top));
+		getContentPane().add(new JScrollPane(bottom));
+		getContentPane().add(secondsBar);
+		getContentPane().add(interpret);
+		
+		//Ready for viewing
+		setVisible(true);
+	}
+	
+	public static void start() {
+		w = new UyjWindow();
+	}
+	
+	private class CodeTimer extends Thread {
+		public CodeTimer() {
+			setName("CodeTimer");
+		}
+		
+		public void run() {
+			try {
+				while(true) {
+				sleep(100);
+				codeTimer++;
+				if(codeTimer==100) {
+					codeTimer=0;
+					if(bottom.getText().split("\n").length>0) {
+						ArrayList<String> lns = new ArrayList<String>();
+						for(String ln : bottom.getText().split("\n")) {
+							lns.add(ln);
+						}
+						if((lns.get(0).trim()+' ').charAt(0)!=' ') {
+							top.setText(top.getText()+"\n"+lns.get(0));
+						}
+						lns.remove(0);
+						if(lns.size()>0) {
+							String txt = lns.get(0);
+							for(int i = 1; i < lns.size(); i++) {
+								txt+="\n"+lns.get(i);
+							}
+							bottom.setText(txt);
+						} else {
+							bottom.setText("");
+						}
+					}
+				}
+				secondsBar.setValue(codeTimer);
+				secondsBar.setString(Integer.toString((100-codeTimer)/10)+" seconds");
+				}
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
--- src/net/termer/esolang/Uyjhmnn.java
+++ src/net/termer/esolang/Uyjhmnn.java
@@ -19,9 +19,16 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
+
+import javax.swing.JOptionPane;
 
 import net.termer.esolang.exception.CommandParseException;
 import net.termer.esolang.exception.DataTypeMismatchException;
@@ -29,10 +36,11 @@ import net.termer.esolang.exception.GotoException;
 import net.termer.esolang.exception.VariableException;
 
 public class Uyjhmnn {
-	private static boolean gui = true;
-	private static boolean ignoreErrors = false;
+	public static boolean gui = true;
+	public static boolean ignoreErrors = false;
 	
 	public static void main(String[] args) {
+		Thread.currentThread().setName("jUyjhmnn");
 		ArrayList<String> flags = new ArrayList<String>();
 		ArrayList<String> arguments = new ArrayList<String>();
 		for(String arg : args) {
@@ -45,8 +53,15 @@ public class Uyjhmnn {
 		if(flags.contains("nogui")) {
 			gui=false;
 		}
+		if(flags.contains("pkg")) {
+			gui=false;
+			System.out.println("Creating package...");
+		}
 		if(flags.contains("ignore-errors")) {
 			ignoreErrors = true;
+		}
+		if(gui) {
+			UyjWindow.start();
 		}
 		if(arguments.size()>0) {
 			try {
@@ -56,7 +71,15 @@ public class Uyjhmnn {
 					script+=(char)fin.read();
 				}
 				fin.close();
-				new Interpreter(script).interpret(ignoreErrors);
+				if(gui) {
+					UyjWindow.w.bottom.setText(script);
+				} else {
+					if(flags.contains("pkg")) {
+						new Uyjhmnn(arguments.get(0));
+					} else {
+						new Interpreter(script).interpret(ignoreErrors);
+					}
+				}
 			} catch(Exception e) {
 				int ln = 0;
 				if(e.getClass()==CommandParseException.class) {
@@ -85,27 +108,94 @@ public class Uyjhmnn {
 	}
 	
 	public static void output(char out) {
-		System.out.print(out);
+		if(gui) {
+			OutputWindow.open();
+			OutputWindow.append(out);
+		} else {
+			System.out.print(out);
+		}
 	}
 	public static void output(String out) {
-		System.out.print(out);
+		if(gui) {
+			OutputWindow.open();
+			OutputWindow.append(out);
+		} else {
+			System.out.print(out);
+		}
 	}
 	public static int inputNumber() throws IOException {
 		int r = 0;
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		while(true) {
-			String ln = in.readLine();		
-			try {
-				r=Integer.parseInt(ln);
-				break;
-			} catch(Exception e) {}
+		if(gui) {
+			while(true) {
+				try {
+					r=Integer.parseInt(JOptionPane.showInputDialog("Please enter a number:"));
+					break;
+				} catch(Exception e) {} 
+			}
+		} else {
+			BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+			while(true) {
+				String ln = in.readLine();		
+				try {
+					r=Integer.parseInt(ln);
+					break;
+				} catch(Exception e) {}
+			}
 		}
 		return r;
 	}
 	public static int inputCharacter() throws IOException {
 		int r = 0;
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		r = in.read();		
+		if(gui) {
+			try {
+				r=(int)JOptionPane.showInputDialog("Please enter a character:").charAt(0);
+			} catch(Exception e) {}
+		} else {
+			BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+			r = in.read();
+		}
 		return r;
+	}
+	
+	public Uyjhmnn(String script) throws IOException {
+		FileOutputStream fos = new FileOutputStream(new File(script+".jar"));
+		InputStream in = getClass().getResourceAsStream("/resources/standalone.jar");
+		BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+		while(reader.ready()) {
+			fos.write(reader.read());
+		}
+		fos.close();
+		addFilesToZip(new File(script+".jar"), new File(script));
+	}
+	
+	public static void addFilesToZip(File zipFile, File file) throws IOException {
+		File tempFile = File.createTempFile(zipFile.getName(), null);
+		tempFile.delete();
+//		boolean renameOk = zipFile.renameTo(tempFile);
+//		if(!renameOk) {
+//			throw new RuntimeException("could not rename file");
+//		}
+		byte[] buf = new byte[1024];
+		ZipInputStream zin = new ZipInputStream(new FileInputStream(tempFile));
+		ZipOutputStream out = new ZipOutputStream(new FileOutputStream(zipFile));
+		ZipEntry entry = zin.getNextEntry();
+		while(entry!=null) {
+			String name = entry.getName();
+			out.putNextEntry(new ZipEntry(file.getName()));
+			int len;
+			while((len = zin.read(buf)) > 0) {
+				out.write(buf, 0, len);
+			}
+		}
+		entry = zin.getNextEntry();
+		zin.close();
+		InputStream in = new FileInputStream(file);
+		out.putNextEntry(new ZipEntry(file.getName()));
+		int len;
+		while((len = in.read(buf)) > 0) {
+			out.write(buf, 0, len);
+		}
+		out.closeEntry();
+		in.close();
 	}
 }
